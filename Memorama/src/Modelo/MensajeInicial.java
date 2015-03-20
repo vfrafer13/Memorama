8aabf0863dde6881a9dde88fe34761e2a6a90cd4
vfrafer13 <vfrafer13@gmail.com>
2015-03-20 08:05:28 -0600

    Bases
    Los archivos base del proyecto.

---
+++ Memorama/src/Modelo/MensajeInicial.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Vanessa
+ */
+public class MensajeInicial extends Mensaje{
+
+    public MensajeInicial(ArrayList<Card> cartas, int turno) {
+        super(cartas, turno);
+    }
+    
+}
